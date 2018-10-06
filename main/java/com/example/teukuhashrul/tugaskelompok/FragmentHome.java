dfaffedb2df09849ea4f1726951d2c9a9c0f5b26
Teuku Hashrul <hasrultk98@gmail.com>
2018-10-06 09:10:58 +0700

    Add files via upload

---
+++ main/java/com/example/teukuhashrul/tugaskelompok/FragmentHome.java
@@ -0,0 +1,42 @@
+package com.example.teukuhashrul.tugaskelompok;
+
+
+import android.os.Bundle;
+
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentHome extends Fragment {
+    private TextView tvHello;
+    private MainActivity activity;
+
+    public FragmentHome() {
+        // Required empty public constructor
+    }
+
+    public static FragmentHome newInstance(MainActivity activity){
+        FragmentHome result = new FragmentHome();
+        result.activity = activity ;
+        return result;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View result = inflater.inflate(R.layout.fragment_fragment_home, container, false);
+        this.tvHello = result.findViewById(R.id.tvHello);
+        this.tvHello.setText("TES HELLO");
+        return result;
+
+    }
+
+}
---
+++ main/java/com/example/teukuhashrul/tugaskelompok/MainActivity.java
@@ -0,0 +1,118 @@
+package com.example.teukuhashrul.tugaskelompok;
+
+import android.os.CountDownTimer;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    protected FragmentGame fragmentGame;
+    protected FragmentHome fragmentHome ;
+
+    protected static final long START_TIME=60000;
+
+    protected TextView tv_timer;
+
+    protected CountDownTimer timer;
+    private long timeLeft=START_TIME;
+
+    protected FragmentManager fragmentManager;
+
+    protected Button btnStart;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.btnStart = this.findViewById(R.id.startGame);
+        this.btnStart.setOnClickListener(this);
+        this.tv_timer=this.findViewById(R.id.timer);
+
+        this.fragmentGame = FragmentGame.newInstance(this);
+        this.fragmentHome = FragmentHome.newInstance(this);
+
+        this.fragmentManager = this.getSupportFragmentManager() ;
+
+        FragmentTransaction fragmentTransaction  = this.fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainer , fragmentHome);
+        fragmentTransaction.commit();
+        updateCountDownText();
+        this.tv_timer.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(this.btnStart == v){
+
+            if(this.btnStart.getText().toString().equals("START")){
+                Log.d("masuk" , "ulala");
+                FragmentTransaction fragmentTransaction = this.fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragmentContainer , fragmentGame);
+                fragmentTransaction.commit();
+
+                this.btnStart.setText("FINISH");
+//                this.startTimer();
+//                this.tv_timer.setVisibility(View.VISIBLE);
+
+            }else{
+                FragmentTransaction fragmentTransaction = this.fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragmentContainer , fragmentHome);
+                fragmentTransaction.commit();
+
+                this.btnStart.setText("START");
+                this.resetTimer();
+            }
+        }
+    }
+
+    public void startTimer(){
+        timer=new CountDownTimer(timeLeft,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeLeft=millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                timer.cancel();
+                //btnStart.setText("Start");
+                timeLeft=START_TIME;
+                fragmentGame.setIsPlay(false);
+                Log.d("isPlay" , fragmentGame.getIsPlay()+"");
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragmentContainer , fragmentHome );
+                fragmentTransaction.commit();
+
+                btnStart.setText(R.string.btn_start);
+                tv_timer.setVisibility(View.INVISIBLE);
+            }
+        }.start();
+        //btnStart.setText("Finish");
+    }
+    public void resetTimer(){
+        timeLeft=START_TIME;
+        timer.cancel();
+        //btnStart.setText("Start");
+        updateCountDownText();
+    }
+    private void updateCountDownText(){
+        int minutes=(int) (timeLeft/1000)/60;
+        int seconds=(int) (timeLeft/1000)%60;
+
+        String timeLeftFormat=String.format(Locale.getDefault(),"%02d:%02d",minutes,seconds);
+        this.tv_timer.setText(timeLeftFormat);
+    }
+
+    private void backToStartFragment(){
+
+    }
+}
